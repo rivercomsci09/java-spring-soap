92b19601503f0ae18a8e716e16e24249283970e0
Tran Truong Giang <43599295+rivercomsci09@users.noreply.github.com>
2019-09-27 23:54:46 +0700

    Create Account.java

---
+++ src/main/java/com/river/dao/model/Account.java
@@ -0,0 +1,23 @@
+package com.river.dao.model;
+
+public class Account {
+
+    private int accountId;
+    private String accountName;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+}
